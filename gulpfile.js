c503997c96d08d9882a6efacc494775031e819fa
Evan Willhite <evan@evanwillhite.com>
2017-03-09 09:46:47 -0600

    added gh-pages deployment

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,13 @@
   const gulpSequence = require('gulp-sequence')
   require('require-dir')('./gulp-tasks')
 
+  var ghPages = require('gulp-gh-pages');
+
+  gulp.task('deploy', ['build'], function() {
+    return gulp.src('./dist/**/*')
+      .pipe(ghPages());
+  });
+
   /**
    * Remove the `dist` folder.
    */
