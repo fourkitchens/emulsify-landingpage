bfeef750a4673f08eec19a39f247d65631b9a401
Randy Oest <oest@amazingrando.com>
2020-02-13 09:55:43 -0500

    Major update

--- gulp-tasks/morgue.js
+++ gulp-tasks/morgue.js
@@ -1,16 +1,12 @@
-/* globals require, process, __dirname */
-
-(function () {
-  'use strict'
-
-  const gulp = require('gulp')
-  const config = require('../gulp-config')
+(() => {
+  const gulp = require('gulp');
+  const config = require('../gulp-config');
 
   /**
    * Move the morgue to `dist`.
    */
-  gulp.task('move-morgue', function () {
-    gulp.src(config.paths.morgue + '/**', { base: './' })
-      .pipe(gulp.dest(config.paths.dist_morgue))
-  })
-})()
+  gulp.task('move-morgue', () => {
+    gulp.src(`${config.paths.morgue}/**`, { base: './' })
+      .pipe(gulp.dest(config.paths.dist_morgue));
+  });
+})();
